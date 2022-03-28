b5e40e258049d9d743ac71e727d6bbc34038b277
rawand1998 <rawandgaradh@gmail.com>
2022-03-28 16:53:58 +0300

    file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { DetailComponentComponent } from './components/detail-component/detail-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    DetailComponentComponent
   ],
   imports: [
     BrowserModule,
