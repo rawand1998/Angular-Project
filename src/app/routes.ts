6ad35ba40801c60a2ef64be53f2ca8e89e3534f2
rawand1998 <rawandgaradh@gmail.com>
2022-03-28 17:33:30 +0300

    route details

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,8 @@
 
 import {HomeComponentComponent} from './components/home-component/home-component.component';
+import {DetailComponentComponent} from './components/detail-component/detail-component.component';
 export const routingTable = [
     { path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path:'home',component: HomeComponentComponent}
-
+  {path:'home',component: HomeComponentComponent},
+  {path:'details/:id',component: DetailComponentComponent},
   ]
