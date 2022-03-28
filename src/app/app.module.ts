9d5f7e0539b8f645d9dabb19181d8f84931b6e2e
rawand1998 <rawandgaradh@gmail.com>
2022-03-28 17:19:14 +0300

    create routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponentComponent } from './components/home-component/home-component.component';
 import { DetailComponentComponent } from './components/detail-component/detail-component.component';
-
+import { routingTable } from './routes';
+import { HttpClientModule } from '@angular/common/http';
+import {CountryserviceService} from './services/countryservice.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +16,11 @@ import { DetailComponentComponent } from './components/detail-component/detail-c
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routingTable),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CountryserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
