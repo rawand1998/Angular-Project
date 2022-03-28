cc20a3c3c389c3efef7d1e2890592d746336ee27
rawand1998 <rawandgaradh@gmail.com>
2022-03-28 17:56:34 +0300

    get details function

--- src/app/components/detail-component/detail-component.component.ts
+++ src/app/components/detail-component/detail-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountryserviceService } from 'src/app/services/countryservice.service';
 
 @Component({
   selector: 'app-detail-component',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-component.component.css']
 })
 export class DetailComponentComponent implements OnInit {
+  userId: any;
+  data: any;
 
-  constructor() { }
+  constructor(private service: CountryserviceService ,private route:ActivatedRoute) { }
+
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.userId=params['id'];
+      console.log(this.userId)
+     this.service.getCountryDetails(this.userId).subscribe(foodDetails=>{this.data=foodDetails})
+    
+     
+    })
   }
 
 }
