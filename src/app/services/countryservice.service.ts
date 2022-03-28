150de8c8ec9bd5f8bdf7658cd60488901f87f30d
rawand1998 <rawandgaradh@gmail.com>
2022-03-28 17:00:02 +0300

    get country function

--- src/app/services/countryservice.service.ts
+++ src/app/services/countryservice.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class CountryserviceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  country="https://restcountries.com/v3.1/all"
+  getAllCountry():Observable<any>{
+    return this.http.get(this.country)
+
+  }
 }
